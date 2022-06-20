27d956f1bcf2f9c8ec629d25a8ec8ba4fdba81e9
DrikaDev <adriguti.adriana@hotmail.com>
2022-06-20 16:25:15 -0300

    Convite.aniversario

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayAbout(){
+    var arrow = document.getElementById('animation');
+        arrow.classList.toggle('rotate');
+    var display = document.getElementById('about');
+        display.classList.toggle('active');
+}
